22539c0355104d9695519b99541282ba11700418
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-03-29 14:43:36 +0200

    Update functions

--- lab/model_selection_and_evaluation_functions.py
+++ lab/model_selection_and_evaluation_functions.py
@@ -8,16 +8,16 @@ from sklearn.metrics import (
 
 
 def create_X_y_from_data_frame(data_frame: pandas.DataFrame, target_column: str):
-    """Return a pandas.DataFrame's without target column and target column"""
+    """Returns a pandas.DataFrame's without target column and target column"""
     return data_frame.drop(columns=[target_column]), data_frame[target_column]
 
 
 def grid_search_hyper_parameters(
-    X, y, model, parameter_grid: dict, scoring_method: str
+    X, y, unfitted_model, parameter_grid: dict, scoring_method: str
 ):
     """Retruns a fitted model and a pandas.DataFrame of GridSearchCV.cv_results"""
     model = GridSearchCV(
-        estimator=model,
+        estimator=unfitted_model,
         param_grid=parameter_grid,
         return_train_score=False,
         scoring=scoring_method,
@@ -42,15 +42,19 @@ def select_search_parameters_and_scores(
     )
 
 
-def search_hyper_parameters(X, y, model, parameter_grid: dict, scoring_method: str):
+def search_hyper_parameters(
+    X, y, unfitted_model, parameter_grid: dict, scoring_method: str
+):
     """Returns a fitted model, a selection of parameters and scores of GridSearchCV.cv_results_, raw GridSearchCV.cv_results_"""
     model, scores = grid_search_hyper_parameters(
-        X, y, model, parameter_grid, scoring_method
+        X, y, unfitted_model, parameter_grid, scoring_method
     )
     return model, select_search_parameters_and_scores(scores, parameter_grid), scores
 
 
-def show_evaluation_metrics(model, X_test, y_test, display_labels=["True", "False"]):
+def show_classification_evaluation_metrics(
+    model, X_test, y_test, display_labels=["True", "False"]
+):
     """Prints classification report and plots confusion matrix"""
     y_pred = model.predict(X_test)
     print(classification_report(y_test, y_pred))
@@ -68,16 +72,16 @@ def search_score_and_evalute_parameters(
     model_parameter_grid: dict,
     scoring_method: str,
 ):
-    """Searches, scores and prints evaluation of a model and it's parameters then returns the model"""
+    """Searches, scores and prints evaluation of a model and it's parameters then returns the model, model_parameters_and_score, model_parameters_and_score_raw"""
     (
         fitted_model,
         model_parameters_and_score,
-        model_score_raw,
+        model_parameters_and_score_raw,
     ) = search_hyper_parameters(
         X_train, y_train, model, model_parameter_grid, scoring_method
     )
-    show_evaluation_metrics(fitted_model, X_test, y_test)
-    return fitted_model, model_parameters_and_score, model_score_raw
+    show_classification_evaluation_metrics(fitted_model, X_test, y_test)
+    return fitted_model, model_parameters_and_score, model_parameters_and_score_raw
 
 
 def create_train_test_eval_split(data_frame: pandas.DataFrame, target_column: str):
